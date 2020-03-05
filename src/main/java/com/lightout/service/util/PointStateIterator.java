be1ee0bf2fa11dcffcda1720969b263421a37954
alexkirnsu <alexkirnsu@gmail.com>
2020-03-05 11:31:33 +0100

    LO-2 Modify point iterator
    In order to simplify PointStateIterator and to follow `hasNext` and `next` methods contract these methods were refactored

--- src/main/java/com/lightout/service/util/PointStateIterator.java
+++ src/main/java/com/lightout/service/util/PointStateIterator.java
@@ -9,14 +9,13 @@ import java.util.List;
 public class PointStateIterator implements Iterator<Point[]> {
     private List<Piece> pieces;
     private Point[] points;
-    private int[] configuration;
+    private int[] cursor;
     private int [] limits;
-    private boolean firstStep = true;
 
     public PointStateIterator(List<Piece> pieces) {
         this.pieces = pieces;
         int size = pieces.size();
-        this.configuration = new int[size];
+        this.cursor = new int[size];
         this.limits = new int[size];
         this.points = new Point[size];
         for (int i = 0; i < size; i++) {
@@ -26,32 +25,28 @@ public class PointStateIterator implements Iterator<Point[]> {
 
     @Override
     public boolean hasNext() {
-        if (firstStep) {
-            firstStep = false;
-            return true;
-        }
-        doStep();
-        return configuration[0] != limits[0];
+        return cursor[0] != limits[0];
     }
 
     @Override
     public Point[] next() {
         fillPoints();
+        doStep();
         return points;
     }
 
     private void doStep() {
-        for (int i = configuration.length - 1; i >= 0 ; i--) {
-            if (++configuration[i] < limits[i]) {
+        for (int i = cursor.length - 1; i >= 0 ; i--) {
+            if (++cursor[i] < limits[i]) {
                 break;
             }
-            configuration[i] = i != 0 ? 0 : configuration[i];
+            cursor[i] = i != 0 ? 0 : cursor[i];
         }
     }
 
     private void fillPoints() {
-        for (int i = 0; i < configuration.length; i++) {
-            points[i] = pieces.get(i).getTopLeftPoint(configuration[i]);
+        for (int i = 0; i < cursor.length; i++) {
+            points[i] = pieces.get(i).getTopLeftPoint(cursor[i]);
         }
     }
 }
